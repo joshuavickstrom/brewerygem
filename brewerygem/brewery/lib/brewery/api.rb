7bb379bd26cbfe7a36c6465db95d9a3b816f3d23
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-01-24 18:00:42 +0000

    updated api,rb

--- brewerygem/brewery/lib/brewery/api.rb
+++ brewerygem/brewery/lib/brewery/api.rb
@@ -1,6 +1,6 @@
 class BreweryGem::API
   def self.list_breweries
-    breweries = RestClient.get("http://api.brewerydb.com/v2/brewery.list?format=json&key=59c865f579f97048514d374fa26ecbf3")
+    breweries = RestClient.get("http://api.brewerydb.com/v2/GET:/breweries/:name&format=json&key=59c865f579f97048514d374fa26ecbf3")
     @brewery_list = JSON.parse(breweries)
   end
 end
